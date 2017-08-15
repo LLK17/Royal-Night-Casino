915a16d87b33ce7f3be0ffe6d49338664e5c8c5d
unknown <kennethtay2017@gmail.com>
2017-08-15 13:32:12 -0400

    balance

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 window.onload = function() {
+  sessionStorage.setItem('balance', Number('1000'))
+  document.getElementById('lockIn').onclick=playLotto;
+  var rng;
+  var playerNum;
+  /* starts calculations for the lottery game*/
+  function playLotto(){
+    stringToNum();
+    generator();
+    winLose();
+    /*Generates Winning number*/
+    function generator(){
+      rng = Math.floor(Math.random(1)*100);
+      //console.log(rng);
+      document.getElementById('winningNum').innerHTML = rng;
+      return rng;
+    }
 
-  document.getElementById('lockIn').onclick=result;
-  document.getElementById('selectedNum');
+    /*converts player's input into a number*/
+    function stringToNum(){
+      playerNum = Number(document.getElementById('selectedNum').value)
+      //console.log(playerNum)
+      return playerNum
+    }
 
-  function result(){
-    var rng = Math.floor(Math.random()*100);
-      console.log(rng);
-    document.getElementById('result').innerHTML = rng;
-      console.log(document.getElementById('result'))
-
-
-}
-
-
+    /*Tells player if they've won or lost*/
+    function winLose(){
+      if (rng === playerNum){
+        document.getElementById('result').innerHTML = 'YOU WON!';
+      }
+      else{
+        document.getElementById('result').innerHTML = 'You Lost...';
+      }
+    }
+  }
 
 };
